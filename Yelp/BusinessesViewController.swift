aaa921ad3625914ba8a0b44ee53dc3a5c0800c5f
Gonzalo Maldonado <elg0nz@gmail.com>
2017-09-23 20:37:23 -0700

    Add TableView to BusinessesViewController

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,52 +8,66 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController {
-    
+class BusinessesViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var navigationBar: UINavigationBar!
     var businesses: [Business]!
-    
+    let searchBar = UISearchBar()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        searchBar.sizeToFit()
+        self.navigationItem.titleView = searchBar
+        searchBar.backgroundColor = self.navigationBar.backgroundColor
+        tableView.dataSource = self
+
         Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
-            
             self.businesses = businesses
             if let businesses = businesses {
                 for business in businesses {
                     print(business.name!)
                     print(business.address!)
                 }
+             self.tableView.reloadData()
             }
-            
-            }
-        )
-        
+        })
+
         /* Example of Yelp search with more search options specified
          Business.searchWithTerm("Restaurants", sort: .distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: Error!) -> Void in
          self.businesses = businesses
-         
          for business in businesses {
          print(business.name!)
          print(business.address!)
          }
          }
          */
-        
     }
-    
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        let business = businesses[indexPath.row]
+        cell.textLabel?.text = business.name
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if self.businesses != nil {
+            return self.businesses.count
+        }
+        return 0
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     /*
      // MARK: - Navigation
-     
      // In a storyboard-based application, you will often want to do a little preparation before navigation
      override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
      // Get the new view controller using segue.destinationViewController.
      // Pass the selected object to the new view controller.
      }
      */
-    
 }
